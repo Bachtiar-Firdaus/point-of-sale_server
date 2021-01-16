4a2736da696927bafa8c023636f608334b60154d
rluciver <rluciver@aol.com>
2021-01-16 22:21:09 +0700

    Menangani Error Validasi Mongoose

--- app/product/controller.js
+++ app/product/controller.js
@@ -7,6 +7,13 @@ async function store(req, res, next) {
     await product.save();
     return res.json(product);
   } catch (error) {
+    if (error && error.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: error.message,
+        fields: error.errors,
+      });
+    }
     next(error);
   }
 }
