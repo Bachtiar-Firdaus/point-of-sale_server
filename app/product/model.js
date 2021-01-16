45475f2ec1ab4696a4d83dc878462c4f77160c98
rluciver <rluciver@aol.com>
2021-01-16 21:52:16 +0700

    Membuat Schema & Model Product

---
+++ app/product/model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose;
+
+const productSchema = Schema(
+  {
+    name: {
+      type: String,
+      minlength: [3, "Panjang nama makanan minimal 3 karakter"],
+      maxlength: [255, "Panjang nama makanan maksimal 255 karakter"],
+      required: [true, "Nama produk harus diisi"],
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+    },
+
+    image_url: String,
+  },
+  { timestamps: true }
+);
+module.exports = model("Product", productSchema);
