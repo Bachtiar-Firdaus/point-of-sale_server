5237a6cfc92679c491090101c1b7bffe796790b8
rluciver <rluciver@aol.com>
2021-01-16 22:12:06 +0700

    Membuat router products dan Penggunaannya

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require("morgan");
 //default express generator
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
+// custom router
+const productRouter = require("./app/product/router");
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, "public")));
 //default express generator
 // app.use("/", indexRouter);
 // app.use("/users", usersRouter);
+//custom router
+app.use("/api", productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ app/product/router.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const productController = require("./controller");
+
+router.post("/products", productController.store);
+module.exports = router;
