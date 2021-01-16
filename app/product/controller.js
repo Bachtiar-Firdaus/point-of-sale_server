27d85a678826b36c2f232bf950ea16c07526de93
rluciver <rluciver@aol.com>
2021-01-16 22:17:45 +0700

    Menangani Kemungkinan Error

--- app/product/controller.js
+++ app/product/controller.js
@@ -1,10 +1,14 @@
 const Product = require("./model");
 
 async function store(req, res, next) {
-  let payload = req.body;
-  let product = new Product(payload);
-  await product.save();
-  return res.json(product);
+  try {
+    let payload = req.body;
+    let product = new Product(payload);
+    await product.save();
+    return res.json(product);
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = { store };
