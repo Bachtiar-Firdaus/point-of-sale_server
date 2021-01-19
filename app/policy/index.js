100b5cc2331dd6e717b2323b02624bcd8a454027
rluciver <rluciver@aol.com>
2021-01-19 16:35:41 +0700

    Pembuatan Role Pembatasan Akses Policy

---
+++ app/policy/index.js
@@ -0,0 +1,26 @@
+const { AbilityBuilder, Ability } = require("@casl/ability");
+
+const policies = {
+  user(user, { can }) {
+    can("view", "Product");
+    can("create", "Order");
+    can("read", "Order", { user_id: user._id });
+    can("read", "Cart", { user_id: user._id });
+    can("update", "Cart", { user_id: user.id });
+  },
+  admin(user, { can }) {
+    can("manage", "all");
+  },
+};
+
+function policiyFor(user) {
+  let builder = new AbilityBuilder();
+  if (user && typeof policies[user.role] === "function") {
+    policies[user.role](user, builder);
+  } else {
+    policies["guest"](user, builder);
+  }
+  return new Ability(builder.rules);
+}
+
+module.exports = { policiyFor };
