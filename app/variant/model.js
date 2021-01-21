1b5a250cd31951f8b6389adb9b24f48dd5453694
rluciver <rluciver@aol.com>
2021-01-21 23:36:25 +0700

    Pembuatan Array Schema Variant beserta Relational Collection Products

---
+++ app/variant/model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose.set("userCreateIndex", true);
+
+const variantSchema = Schema(
+  {
+    //relational collection
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+
+    name: [
+      {
+        type: String,
+        minlength: [1, "Nama Variant minimal 1 karakter"],
+        required: [true, "Nama Variant Wajib Di isi"],
+      },
+    ],
+    stock: [
+      {
+        type: Number,
+        minlength: [1, "Jumlah Stock minimal 1"],
+        required: [true, "Stock Wajib Di Isi"],
+      },
+    ],
+  },
+  { Timestamp: true }
+);
+
+module.exports = model("Variant", variantSchema);
