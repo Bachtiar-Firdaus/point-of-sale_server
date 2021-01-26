23482421fd90ac45b93356c0a089ef5ab31b767f
rluciver <rluciver@aol.com>
2021-01-27 04:20:18 +0700

    Pembuatan Hooks Triger After SAVE Order pengurangan Stock Variant

--- app/order/backup.js
+++ app/order/backup.js
@@ -32,7 +32,10 @@ const orderSchema = Schema({
 orderSchema.plugin(AutoIncrement, { inc_field: "order_number" });
 
 orderSchema.post("save", async function () {
+  // let idVariant = [];
   let idUser = this.user;
+  // let valueCartVariantOption = [];
+  // let valueQty = [];
   await Cart.find({ user: idUser }).then(async (dataCart) => {
     dataCart.forEach((data) => {
       Product.findOne({ _id: data._id }).then((res) => {
@@ -42,6 +45,7 @@ orderSchema.post("save", async function () {
               option.stock = option.stock - data.qty;
             }
           });
+
           console.log("variant >>>>>>>>>>>");
           console.log(variant);
 
@@ -50,6 +54,33 @@ orderSchema.post("save", async function () {
       });
     });
   });
+  // await Product.find({ _id: this.orders }).then((dataProduct) => {
+  //   dataProduct.map(async (itmProduct) => {
+  //     idVariant.push(itmProduct.variant[0]);
+  //   });
+  // });
+  // let idVariantOption = [];
+  // await Variant.find({ _id: idVariant }).then((dataVariant) => {
+  //   dataVariant.map((itmVariant) => {
+  //     itmVariant.option.map(async (itmOption) => {
+  //       valueCartVariantOption.forEach((dataOption) => {
+  //         if (itmOption.name === dataOption) {
+  //           idVariantOption.push(itmOption._id);
+  //         }
+  //       });
+  //     });
+  //   });
+  // });
+  // console.log(
+  //   "idVariant :",
+  //   idVariant,
+  //   "idVariantOption :",
+  //   idVariantOption,
+  //   "valueCartVariantOption :",
+  //   valueCartVariantOption,
+  //   "valueQty :",
+  //   valueQty
+  // );
 });
 
 module.exports = model("Order", orderSchema);
