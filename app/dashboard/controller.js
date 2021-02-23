8b183f1c609c4f3d03b4befb9d8940370ea0228f
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-02-23 20:12:27 +0700

    Penambahan Endpoint Get Statistics Product dan Penambahan Response Error Pada endpoint weeklyIncome

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -86,6 +86,11 @@ function weeklyIncome(req, res, next) {
     } else if (checkDay === "Saturday") {
       start = `${indicatorDay}${valueDay - 5}`;
       end = `${indicatorDay}${valueDay + 1}`;
+    } else {
+      return res.json({
+        error: 1,
+        message: "Hari Minggu Tidak Ada Data",
+      });
     }
     let weeklyIncome = 0;
     History.find({ date: { $gte: `${start}`, $lt: `${end}` } }).then(
@@ -208,10 +213,172 @@ async function bestSeller(req, res, next) {
     next(error);
   }
 }
+function salesStatisticsProducts(req, res, next) {
+  try {
+    if (!req.user) {
+      return res.json({
+        error: 1,
+        message: "Anda Belum Login atau Token Expired",
+      });
+    }
+    let policy = policyFor(req.user);
+    if (!policy.can("manage", "all")) {
+      return res.json({
+        error: 1,
+        message: "Anda Tidak Memiliki Akses Untuk Melihat BestSeller",
+      });
+    }
+    let { date = "", product = "" } = req.query;
+    let valueDate = "";
+    valueDate = date;
+    if (!date.length) {
+      valueDate = moment().format("YYYY-MM-DD");
+    }
+    let checkDay = moment(valueDate).format("dddd");
+    let stringDay = valueDate.toString();
+    let valueDay = stringDay.substr(8, 2);
+    let indicatorDay = stringDay.substr(0, 8);
+    valueDay = parseInt(valueDay);
+    let start = "";
+    let end = "";
+    let dateMonday = "";
+    let dateTuesday = "";
+    let dateWednesday = "";
+    let dateThursday = "";
+    let dateFriday = "";
+    let dateSaturday = "";
+
+    if (checkDay === "Monday") {
+      start = valueDate;
+      end = `${indicatorDay}${valueDay + 6}`;
+
+      dateMonday = `${indicatorDay}${valueDay}`;
+      dateTuesday = `${indicatorDay}${valueDay + 1}`;
+      dateWednesday = `${indicatorDay}${valueDay + 2}`;
+      dateThursday = `${indicatorDay}${valueDay + 3}`;
+      dateFriday = `${indicatorDay}${valueDay + 4}`;
+      dateSaturday = `${indicatorDay}${valueDay + 5}`;
+    } else if (checkDay === "Tuesday") {
+      start = `${indicatorDay}${valueDay - 1}`;
+      end = `${indicatorDay}${valueDay + 5}`;
+
+      dateMonday = `${indicatorDay}${valueDay - 1}`;
+      dateTuesday = `${indicatorDay}${valueDay}`;
+      dateWednesday = `${indicatorDay}${valueDay + 1}`;
+      dateThursday = `${indicatorDay}${valueDay + 2}`;
+      dateFriday = `${indicatorDay}${valueDay + 3}`;
+      dateSaturday = `${indicatorDay}${valueDay + 4}`;
+    } else if (checkDay === "Wednesday") {
+      start = `${indicatorDay}${valueDay - 2}`;
+      end = `${indicatorDay}${valueDay + 4}`;
+
+      dateMonday = `${indicatorDay}${valueDay - 2}`;
+      dateTuesday = `${indicatorDay}${valueDay - 1}`;
+      dateWednesday = `${indicatorDay}${valueDay}`;
+      dateThursday = `${indicatorDay}${valueDay + 1}`;
+      dateFriday = `${indicatorDay}${valueDay + 2}`;
+      dateSaturday = `${indicatorDay}${valueDay + 3}`;
+    } else if (checkDay === "Thursday") {
+      start = `${indicatorDay}${valueDay - 3}`;
+      end = `${indicatorDay}${valueDay + 3}`;
+
+      dateMonday = `${indicatorDay}${valueDay - 3}`;
+      dateTuesday = `${indicatorDay}${valueDay - 2}`;
+      dateWednesday = `${indicatorDay}${valueDay - 1}`;
+      dateThursday = `${indicatorDay}${valueDay}`;
+      dateFriday = `${indicatorDay}${valueDay + 1}`;
+      dateSaturday = `${indicatorDay}${valueDay + 2}`;
+    } else if (checkDay === "Friday") {
+      start = `${indicatorDay}${valueDay - 4}`;
+      end = `${indicatorDay}${valueDay + 2}`;
+
+      dateMonday = `${indicatorDay}${valueDay - 4}`;
+      dateTuesday = `${indicatorDay}${valueDay - 3}`;
+      dateWednesday = `${indicatorDay}${valueDay - 2}`;
+      dateThursday = `${indicatorDay}${valueDay - 1}`;
+      dateFriday = `${indicatorDay}${valueDay}`;
+      dateSaturday = `${indicatorDay}${valueDay + 1}`;
+    } else if (checkDay === "Saturday") {
+      start = `${indicatorDay}${valueDay - 5}`;
+      end = `${indicatorDay}${valueDay + 1}`;
+
+      dateMonday = `${indicatorDay}${valueDay - 5}`;
+      dateTuesday = `${indicatorDay}${valueDay - 4}`;
+      dateWednesday = `${indicatorDay}${valueDay - 3}`;
+      dateThursday = `${indicatorDay}${valueDay - 2}`;
+      dateFriday = `${indicatorDay}${valueDay - 1}`;
+      dateSaturday = `${indicatorDay}${valueDay}`;
+    } else {
+      return res.json({
+        error: 1,
+        message: "Hari Minggu Tidak Ada Data",
+      });
+    }
+    let Monday = 0;
+    let Tuesday = 0;
+    let Wednesday = 0;
+    let Thursday = 0;
+    let Friday = 0;
+    let Saturday = 0;
+
+    let bultStatistics = [];
+    History.find({ date: { $gte: `${start}`, $lt: `${end}` } }).then(
+      (check_date) => {
+        check_date.map((dataStatistics) => {
+          dataStatistics.orders.map((built) => {
+            bultStatistics.push({
+              date: dataStatistics.date,
+              name: built.Product,
+              sigma: built.Sub_Total_Belanja,
+            });
+          });
+        });
+        bultStatistics.map((rebuilt) => {
+          if (rebuilt.date === dateMonday && rebuilt.name === product) {
+            Monday = Monday + rebuilt.sigma;
+          } else if (rebuilt.date === dateTuesday && rebuilt.name === product) {
+            Tuesday = Tuesday + rebuilt.sigma;
+          } else if (
+            rebuilt.date === dateWednesday &&
+            rebuilt.name === product
+          ) {
+            Wednesday = Wednesday + rebuilt.sigma;
+          } else if (
+            rebuilt.date === dateThursday &&
+            rebuilt.name === product
+          ) {
+            Thursday = Thursday + rebuilt.sigma;
+          } else if (rebuilt.date === dateFriday && rebuilt.name === product) {
+            Friday = Friday + rebuilt.sigma;
+          } else if (
+            rebuilt.date === dateSaturday &&
+            rebuilt.name === product
+          ) {
+            Saturday = Saturday + rebuilt.sigma;
+          }
+        });
+        return res.json({
+          message: "succes",
+          data: [
+            { day: "Monday", value: Monday },
+            { day: "Tuesday", value: Tuesday },
+            { day: "Wednesday", value: Wednesday },
+            { day: "Thursday", value: Thursday },
+            { day: "Friday", value: Friday },
+            { day: "Saturday", value: Saturday },
+          ],
+        });
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+}
 module.exports = {
   todaysIncome,
   monthlyIncome,
   weeklyIncome,
   userActive,
   bestSeller,
+  salesStatisticsProducts,
 };
--- app/dashboard/router.js
+++ app/dashboard/router.js
@@ -7,5 +7,9 @@ router.get("/dashboard/monthly-income", dashboardController.monthlyIncome);
 router.get("/dashboard/weekly-income", dashboardController.weeklyIncome);
 router.get("/dashboard/user-active", dashboardController.userActive);
 router.get("/dashboard/best-seller", dashboardController.bestSeller);
+router.get(
+  "/dashboard/sales-statistics-products",
+  dashboardController.salesStatisticsProducts
+);
 
 module.exports = router;
