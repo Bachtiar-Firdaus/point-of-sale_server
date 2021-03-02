e862577bcf522a2c0aa1dbfc5a50160e51f50af5
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-03-02 16:55:44 +0700

    Penambahan Security Pada Router Variant

--- app/variant/router.js
+++ app/variant/router.js
@@ -1,22 +1,25 @@
 const router = require("express").Router();
 const multer = require("multer");
+const { securityCek } = require("../middleware/security");
 const { validateVariant } = require("../middleware/validator");
 const variantController = require("./controller");
 
-router.get("/variant", variantController.index);
+router.get("/variant", securityCek, variantController.index);
 router.post(
   "/variant",
+  securityCek,
   validateVariant,
   multer().none(),
   variantController.variant
 );
 router.put(
   "/variant/:id",
+  securityCek,
   validateVariant,
   multer().none(),
   variantController.update
 );
-router.get("/variant/:id", variantController.singleVariant);
-router.delete("/variant/:id", variantController.destroy);
+router.get("/variant/:id", securityCek, variantController.singleVariant);
+router.delete("/variant/:id", securityCek, variantController.destroy);
 
 module.exports = router;
