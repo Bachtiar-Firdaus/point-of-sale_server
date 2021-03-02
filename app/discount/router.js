034c58f445b3c7631bdca49305535cc1ae3894b6
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-03-02 16:26:24 +0700

    Penambahan Security Pada Router Discount

--- app/discount/router.js
+++ app/discount/router.js
@@ -1,31 +1,35 @@
 const router = require("express").Router();
 const multer = require("multer");
+const { securityCek } = require("../middleware/security");
 const {
   validateDiscount,
   validateProductToDiscount,
 } = require("../middleware/validator");
 const discountController = require("./controller");
 
-router.get("/discount", discountController.index);
-router.get("/discount/:id", discountController.singgleDiscount);
+router.get("/discount", securityCek, discountController.index);
+router.get("/discount/:id", securityCek, discountController.singgleDiscount);
 router.post(
   "/discount",
+  securityCek,
   validateDiscount,
   multer().none(),
   discountController.createDiscount
 );
 router.post(
   "/discount/push-product/:id",
+  securityCek,
   validateProductToDiscount,
   multer().none(),
   discountController.addProductToDiscount
 );
 router.put(
   "/discount/:id",
+  securityCek,
   validateDiscount,
   multer().none(),
   discountController.updateDiscount
 );
-router.delete("/discount/:id", discountController.destroyDiscount);
+router.delete("/discount/:id", securityCek, discountController.destroyDiscount);
 
 module.exports = router;
