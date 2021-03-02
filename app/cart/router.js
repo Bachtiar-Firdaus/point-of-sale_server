a7dc1454b81c7dfefb6558b52286132a697fd041
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-03-02 16:14:28 +0700

    Penambahan Security Pada Router

--- app/cart/router.js
+++ app/cart/router.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 const multer = require("multer");
+const { securityCek } = require("../middleware/security");
 const { validateCart } = require("../middleware/validator");
 const cartController = require("./controller");
 
-router.get("/cart", cartController.index);
-router.put("/cart", validateCart, multer().none(), cartController.cart);
+router.get("/cart", securityCek, cartController.index);
+router.put(
+  "/cart",
+  securityCek,
+  validateCart,
+  multer().none(),
+  cartController.cart
+);
 
 module.exports = router;
