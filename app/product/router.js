821c6fac3f182cc5ed08e548573914c75fa0220a
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-03-02 16:32:56 +0700

    Penambahan Security Pada Router Product

--- app/product/router.js
+++ app/product/router.js
@@ -1,23 +1,26 @@
 const router = require("express").Router();
 const multer = require("multer");
 const os = require("os");
+const { securityCek } = require("../middleware/security");
 const productController = require("./controller");
 
 router.post(
   "/products",
+  securityCek,
   multer({ dest: os.tmpdir() }).single("image"),
   productController.store
 );
 
-router.get("/products", productController.index);
-router.get("/products/best-seller", productController.bestSeller);
-router.get("/products/:id", productController.singgleProduct);
+router.get("/products", securityCek, productController.index);
+router.get("/products/best-seller", securityCek, productController.bestSeller);
+router.get("/products/:id", securityCek, productController.singgleProduct);
 
 router.put(
   "/products/:id",
+  securityCek,
   multer({ dest: os.tmpdir() }).single("image"),
   productController.update
 );
 
-router.delete("/products/:id", productController.destroy);
+router.delete("/products/:id", securityCek, productController.destroy);
 module.exports = router;
