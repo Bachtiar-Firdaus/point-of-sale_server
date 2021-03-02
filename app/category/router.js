0681a793249b4ec3db207e8dff8f571095187539
Bachtiar-Firdaus <id.Bachtiar.firdaus@gmail.com>
2021-03-02 16:19:46 +0700

    Penambahan Security Pada Router Category

--- app/category/router.js
+++ app/category/router.js
@@ -1,22 +1,25 @@
 const router = require("express").Router();
 const multer = require("multer");
+const { securityCek } = require("../middleware/security");
 const { validateCategory } = require("../middleware/validator");
 const categoryController = require("./controller");
 
-router.get("/categories", categoryController.index);
-router.get("/categories/:id", categoryController.singgleCategory);
+router.get("/categories", securityCek, categoryController.index);
+router.get("/categories/:id", securityCek, categoryController.singgleCategory);
 router.post(
   "/categories",
+  securityCek,
   validateCategory,
   multer().none(),
   categoryController.category
 );
 router.put(
   "/categories/:id",
+  securityCek,
   validateCategory,
   multer().none(),
   categoryController.update
 );
-router.delete("/categories/:id", categoryController.destroy);
+router.delete("/categories/:id", securityCek, categoryController.destroy);
 
 module.exports = router;
